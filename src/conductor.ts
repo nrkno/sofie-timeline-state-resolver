0f75f209b258cb0902f15ef02b8150920957b172
Balte de Wit <balte.dewit@gmail.com>
2018-02-26 19:16:36 +0100

    organise Constructor class

--- src/conductor.ts
+++ src/conductor.ts
@@ -1,30 +1,28 @@
 import * as _ from "underscore"
-import {Resolver, TimelineObject} from "superfly-timeline"
+import {Resolver} from "superfly-timeline"
 
 import {Device, DeviceCommand} from "./devices/device"
 import {CasparCGDevice} from "./devices/casparCG"
+import {TimelineObject} from "./timeline/timeline"
 
 const LOOKAHEADTIME = 5000;
 
-/**
-
-*/
 export interface TimelineContentObject extends TimelineObject {
 
 }
+
 export interface Mapping {
 	[layerName:string]: {
 		device: string,
 		[key:string]: any
 	}
 }
+
 export interface Device {
 
 }
 
 export interface ConductorOptions {
-	
-
 	devices: {
 		[deviceName:string]: {
 			type: DeviceTypes.CASPARCG
@@ -32,6 +30,7 @@ export interface ConductorOptions {
 	},
 	initializeAsClear: true // don't do any initial checks with devices to determine state, instead assume that everything is clear, black and quiet
 }
+
 export enum DeviceTypes {
 	CASPARCG=0
 }
@@ -60,22 +59,24 @@ export class Conductor {
 		return this._initializeDevices();
 	}
 
-	setTimeline(timeline:Array<TimelineContentObject>) {
-		// Set the updated timeline (will cause the timeline to re-resolve, and send appropriate commands)
-		this._timeline = timeline;
-		this._resolveTimeline();
+	
+	get mapping():Mapping {
+		return this._mapping;
 	}
-	setMapping(mapping:Mapping) {
+	set mapping(mapping:Mapping) {
 		// Set mapping
 		// re-resolve timeline
 		this._mapping = mapping;
 		this._resolveTimeline();
 	}
-	get mapping():Mapping {
-		return this._mapping;
-	}
+
 	get timeline():Array<TimelineContentObject> {
 		return this._timeline;
+	}
+	set timeline(timeline:TimelineContentObject[]) {
+		// Set the updated timeline (will cause the timeline to re-resolve, and send appropriate commands)
+		this._timeline = timeline;
+		this._resolveTimeline();
 	}
 
 	private _initializeDevices():Promise<any> {
