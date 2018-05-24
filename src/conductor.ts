d2f98d435a712e0c3fbf17d4bc9201f9043bad45
Johan Nyman <johan@nytamin.se>
2018-05-24 09:14:39 +0200

    Fix: wrap the code in try/catch clause

--- src/conductor.ts
+++ src/conductor.ts
@@ -83,7 +83,7 @@ export class Conductor extends EventEmitter {
 		if (options.autoInit) {
 			this.init()
 			.catch((e) => {
-				console.log('Error during auto-init: ', e)
+				this.emit('error','Error during auto-init: ', e)
 			})
 		}
 
@@ -102,9 +102,7 @@ export class Conductor extends EventEmitter {
 	 */
 	public getCurrentTime () {
 
-		// TODO: Implement time sync, NTP procedure etc...
 		if (this._getCurrentTime) {
-			// console.log(this._getCurrentTime)
 			// return 0
 			return this._getCurrentTime()
 		} else {
@@ -147,39 +145,44 @@ export class Conductor extends EventEmitter {
 		return this.devices[deviceId]
 	}
 	public addDevice (deviceId, deviceOptions: DeviceOptions): Promise<Device> {
-		let newDevice: Device
+		try {
+			let newDevice: Device
 
-		if (deviceOptions.type === DeviceType.ABSTRACT) {
-			// Add Abstract device:
-			newDevice = new AbstractDevice(deviceId, deviceOptions, {
-				getCurrentTime: () => { return this.getCurrentTime() }
-			}) as Device
-		} else if (deviceOptions.type === DeviceType.CASPARCG) {
-			// Add CasparCG device:
-			newDevice = new CasparCGDevice(deviceId, deviceOptions, {
-				getCurrentTime: () => { return this.getCurrentTime() }
-			}) as Device
-		} else if (deviceOptions.type === DeviceType.ATEM) {
-			newDevice = new AtemDevice(deviceId, deviceOptions, {
-				getCurrentTime: () => { return this.getCurrentTime() }
-			}) as Device
-		} else if (deviceOptions.type === DeviceType.HTTPSEND) {
-			newDevice = new HttpSendDevice(deviceId, deviceOptions, {
-				getCurrentTime: () => { return this.getCurrentTime() }
-			}) as Device
-		} else {
-			return Promise.reject('No matching device type for "' + deviceOptions.type + '" found')
+			if (deviceOptions.type === DeviceType.ABSTRACT) {
+				// Add Abstract device:
+				newDevice = new AbstractDevice(deviceId, deviceOptions, {
+					getCurrentTime: () => { return this.getCurrentTime() }
+				}) as Device
+			} else if (deviceOptions.type === DeviceType.CASPARCG) {
+				// Add CasparCG device:
+				newDevice = new CasparCGDevice(deviceId, deviceOptions, {
+					getCurrentTime: () => { return this.getCurrentTime() }
+				}) as Device
+			} else if (deviceOptions.type === DeviceType.ATEM) {
+				newDevice = new AtemDevice(deviceId, deviceOptions, {
+					getCurrentTime: () => { return this.getCurrentTime() }
+				}) as Device
+			} else if (deviceOptions.type === DeviceType.HTTPSEND) {
+				newDevice = new HttpSendDevice(deviceId, deviceOptions, {
+					getCurrentTime: () => { return this.getCurrentTime() }
+				}) as Device
+			} else {
+				return Promise.reject('No matching device type for "' + deviceOptions.type + '" found')
+			}
+
+			this.emit('info', 'Initializing ' + DeviceType[deviceOptions.type] + '...')
+			this.devices[deviceId] = newDevice
+			newDevice.mapping = this.mapping
+
+			return newDevice.init(deviceOptions.options)
+			.then(() => {
+				console.log(DeviceType[deviceOptions.type] + ' initialized!')
+				return newDevice
+			})
+		} catch (e) {
+			this.emit('error', e)
+			return Promise.reject(e)
 		}
-
-		console.log('Initializing ' + DeviceType[deviceOptions.type] + '...')
-		this.devices[deviceId] = newDevice
-		newDevice.mapping = this.mapping
-
-		return newDevice.init(deviceOptions.options)
-		.then(() => {
-			console.log(DeviceType[deviceOptions.type] + ' initialized!')
-			return newDevice
-		})
 	}
 	public removeDevice (deviceId: string): Promise<void> {
 		let device = this.devices[deviceId]
@@ -241,136 +244,145 @@ export class Conductor extends EventEmitter {
 	 * Resolves the timeline for the next resolve-time, generates the commands and passes on the commands.
 	 */
 	private _resolveTimeline () {
-		if (!this._isInitialized) {
-			console.log('TSR is not initialized yet')
-			return
-		}
-		const now = this.getCurrentTime()
-		let resolveTime: number = this._nextResolveTime || now
+		let timeUntilNextResolve = LOOKAHEADTIME
 
-		console.log('resolveTimeline ' + resolveTime + ' -----------------------------')
+		try {
 
-		if (resolveTime > now + LOOKAHEADTIME) {
-			console.log('Too far ahead (' + resolveTime + ')')
-			this._triggerResolveTimeline(LOOKAHEADTIME)
-			return
-		}
-
-		this._fixNowObjects(resolveTime)
-
-		let timeline = this.timeline
-		_.each(timeline, (o) => {
-			delete o['parent']
-			if (o.isGroup) {
-				if (o.content.objects) {
-					_.each(o.content.objects, (o2) => {
-						delete o2['parent']
-					})
-				}
+			if (!this._isInitialized) {
+				console.log('TSR is not initialized yet')
+				return
 			}
-		})
-		// @ts-ignore
-		// console.log('timeline', JSON.stringify(timeline, ' ', 2))
+			const now = this.getCurrentTime()
+			let resolveTime: number = this._nextResolveTime || now
 
-		// Generate the state for that time:
-		let tlState = Resolver.getState(clone(timeline), resolveTime)
+			console.log('resolveTimeline ' + resolveTime + ' -----------------------------')
 
-		_.each(tlState.LLayers, (obj) => {
-			delete obj.parent
-		})
-		_.each(tlState.GLayers, (obj) => {
-			delete obj.parent
-		})
+			if (resolveTime > now + LOOKAHEADTIME) {
+				console.log('Too far ahead (' + resolveTime + ')')
+				this._triggerResolveTimeline(LOOKAHEADTIME)
+				return
+			}
 
-		// @ts-ignore
-		// console.log('tlState', JSON.stringify(tlState.LLayers,' ', 2))
+			this._fixNowObjects(resolveTime)
 
-		// Split the state into substates that are relevant for each device
-		let getFilteredLayers = (layers: TimelineState['LLayers'], device: Device) => {
-			let filteredState = {}
-			_.each(layers, (o: TimelineResolvedObject, layerId: string) => {
-				let mapping: Mapping = this._mapping[o.LLayer + '']
-				if (mapping) {
-					if (
-						mapping.deviceId === device.deviceId &&
-						mapping.device === device.deviceType
-					) {
-						filteredState[layerId] = o
+			let timeline = this.timeline
+			_.each(timeline, (o) => {
+				delete o['parent']
+				if (o.isGroup) {
+					if (o.content.objects) {
+						_.each(o.content.objects, (o2) => {
+							delete o2['parent']
+						})
 					}
 				}
 			})
-			return filteredState
-		}
-		_.each(this.devices, (device: Device/*, deviceName: string*/) => {
+			// @ts-ignore
+			// console.log('timeline', JSON.stringify(timeline, ' ', 2))
 
-			// The subState contains only the parts of the state relevant to that device
-			let subState: TimelineState = {
-				time: tlState.time,
-				LLayers: getFilteredLayers(tlState.LLayers, device),
-				GLayers: getFilteredLayers(tlState.GLayers, device)
-			}
-			// console.log('State of device ' + device.deviceName, tlState.LLayers )
-			// Pass along the state to the device, it will generate its commands and execute them:
-			try {
-				device.handleState(subState)
-			} catch (e) {
-				console.log('Error in device "' + device.deviceId + '"', e)
-			}
-		})
+			// Generate the state for that time:
+			let tlState = Resolver.getState(clone(timeline), resolveTime)
 
-		// Now that we've handled this point in time, it's time to determine what the next point in time is:
-
-		// console.log(tlState.time)
-		const timelineWindow = Resolver.getTimelineInWindow(timeline, tlState.time, tlState.time + LOOKAHEADTIME)
-
-		const nextEvents = Resolver.getNextEvents(timelineWindow, tlState.time + MINTIMEUNIT, 1)
-
-		let timeUntilNextResolve = LOOKAHEADTIME
-
-		const now2 = this.getCurrentTime()
-		if (nextEvents.length) {
-			let nextEvent = nextEvents[0]
-
-			// console.log('nextEvent', nextEvent)
-
-			timeUntilNextResolve = Math.max(MINTRIGGERTIME,
-				Math.min(LOOKAHEADTIME,
-					(nextEvent.time - now2) - PREPARETIME
-				)
-			)
-
-			// resolve at nextEvent.time next time:
-			this._nextResolveTime = nextEvent.time
-
-		} else {
-			// there's nothing ahead in the timeline
-
-			// Tell the devices that the future is clear:
-			_.each(this.devices, (device: Device) => {
-				device.clearFuture(tlState.time)
+			_.each(tlState.LLayers, (obj) => {
+				delete obj.parent
+			})
+			_.each(tlState.GLayers, (obj) => {
+				delete obj.parent
 			})
 
-			// resolve at "now" then next time:
-			this._nextResolveTime = 0
-		}
-		// Special function: send callback to Core
-		_.each (tlState.GLayers, (o: TimelineResolvedObject) => {
-			if (o.content.callBack) {
-				// this._doOnTime.queue(resolveTime, o.id, o.content.callBack, o.content.callBackData)
-				// this._doOnTime.queue(o.resolved.startTime, o.id, o.content.callBack, o.content.callBackData)
-				this._doOnTime.queue(o.resolved.startTime, () => {
-					this.emit('timelineCallback',
-						o.resolved.startTime,
-						o.id,
-						o.content.callBack,
-						o.content.callBackData
-					)
-				})
-			}
-		})
+			// @ts-ignore
+			// console.log('tlState', JSON.stringify(tlState.LLayers,' ', 2))
 
-		// console.log('this._nextResolveTime', this._nextResolveTime)
-		this._triggerResolveTimeline(timeUntilNextResolve)
+			// Split the state into substates that are relevant for each device
+			let getFilteredLayers = (layers: TimelineState['LLayers'], device: Device) => {
+				let filteredState = {}
+				_.each(layers, (o: TimelineResolvedObject, layerId: string) => {
+					let mapping: Mapping = this._mapping[o.LLayer + '']
+					if (mapping) {
+						if (
+							mapping.deviceId === device.deviceId &&
+							mapping.device === device.deviceType
+						) {
+							filteredState[layerId] = o
+						}
+					}
+				})
+				return filteredState
+			}
+			_.each(this.devices, (device: Device/*, deviceName: string*/) => {
+
+				// The subState contains only the parts of the state relevant to that device
+				let subState: TimelineState = {
+					time: tlState.time,
+					LLayers: getFilteredLayers(tlState.LLayers, device),
+					GLayers: getFilteredLayers(tlState.GLayers, device)
+				}
+				// console.log('State of device ' + device.deviceName, tlState.LLayers )
+				// Pass along the state to the device, it will generate its commands and execute them:
+				try {
+					device.handleState(subState)
+				} catch (e) {
+					console.log('Error in device "' + device.deviceId + '"', e)
+				}
+			})
+
+			// Now that we've handled this point in time, it's time to determine what the next point in time is:
+
+			// console.log(tlState.time)
+			const timelineWindow = Resolver.getTimelineInWindow(timeline, tlState.time, tlState.time + LOOKAHEADTIME)
+
+			const nextEvents = Resolver.getNextEvents(timelineWindow, tlState.time + MINTIMEUNIT, 1)
+
+			const now2 = this.getCurrentTime()
+			if (nextEvents.length) {
+				let nextEvent = nextEvents[0]
+
+				// console.log('nextEvent', nextEvent)
+
+				timeUntilNextResolve = Math.max(MINTRIGGERTIME,
+					Math.min(LOOKAHEADTIME,
+						(nextEvent.time - now2) - PREPARETIME
+					)
+				)
+
+				// resolve at nextEvent.time next time:
+				this._nextResolveTime = nextEvent.time
+
+			} else {
+				// there's nothing ahead in the timeline
+
+				// Tell the devices that the future is clear:
+				_.each(this.devices, (device: Device) => {
+					device.clearFuture(tlState.time)
+				})
+
+				// resolve at "now" then next time:
+				this._nextResolveTime = 0
+			}
+			// Special function: send callback to Core
+			_.each (tlState.GLayers, (o: TimelineResolvedObject) => {
+				if (o.content.callBack) {
+					// this._doOnTime.queue(resolveTime, o.id, o.content.callBack, o.content.callBackData)
+					// this._doOnTime.queue(o.resolved.startTime, o.id, o.content.callBack, o.content.callBackData)
+					this._doOnTime.queue(o.resolved.startTime, () => {
+						this.emit('timelineCallback',
+							o.resolved.startTime,
+							o.id,
+							o.content.callBack,
+							o.content.callBackData
+						)
+					})
+				}
+			})
+		} catch (e) {
+			this.emit('error', e)
+		}
+
+		try {
+			// console.log('this._nextResolveTime', this._nextResolveTime)
+			this._triggerResolveTimeline(timeUntilNextResolve)
+		} catch (e) {
+			this.emit('error', e)
+		}
 	}
 
 	private _fixNowObjects (now: number) {
