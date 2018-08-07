3e950bc5415495192e1290a7ef2fe6d6c7c83c12
Balte de Wit <balte.dewit@gmail.com>
2018-08-07 14:54:31 +0200

    chore: update dependencies

--- src/devices/atem.ts
+++ src/devices/atem.ts
@@ -250,7 +250,7 @@ export class AtemDevice extends Device {
 				}
 			}
 		}
-		for (let i = 0; i < this._device.state.video.downstreamKeyers.length; i++) {
+		for (let i = 0; i < Object.keys(this._device.state.video.downstreamKeyers).length; i++) {
 			deviceState.video.downstreamKeyers[i] = JSON.parse(JSON.stringify(StateDefault.Video.DownStreamKeyer))
 		}
 		for (let i = 0; i < this._device.state.info.capabilities.auxilliaries; i++) {
