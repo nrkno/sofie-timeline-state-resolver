069a4b226c96928d98a1ebc30076e133dd83a0ea
Johan Nyman <johan@nytamin.se>
2018-09-21 15:02:10 +0200

    feat: restart CasparCG from TSR

--- src/index.ts
+++ src/index.ts
@@ -4,5 +4,6 @@ export * from './devices/mapping'
 
 // let myConductor = new Conductor();
 export { DeviceOptions } from './devices/device'
+export { CasparCGDevice } from './devices/casparCG'
 
 // @todo remove this line of comment
