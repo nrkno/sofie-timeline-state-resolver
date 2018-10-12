86536f36c3dfbe7530c688ae9c3dde75f93faf06
Julian Waller <julian@superfly.tv>
2018-10-12 15:57:25 +0100

    chore: Convert some Device methods to abstract

--- src/devices/device.ts
+++ src/devices/device.ts
@@ -55,13 +55,7 @@ export abstract class Device extends EventEmitter implements IDevice {
 			this._getCurrentTime = options.getCurrentTime
 		}
 	}
-	init (connectionOptions: any): Promise<boolean> {
-		// connect to the device, resolve the promise when ready.
-		connectionOptions = connectionOptions // ts-ignore
-		throw new Error('This class method must be replaced by the Device class!')
-
-		// return Promise.resolve(true)
-	}
+	abstract init (connectionOptions: any): Promise<boolean>
 	terminate (): Promise<boolean> {
 		return Promise.resolve(true)
 	}
@@ -70,23 +64,10 @@ export abstract class Device extends EventEmitter implements IDevice {
 		return Date.now()
 	}
 
-	handleState (newState: TimelineState) {
-		// Handle this new state, at the point in time specified
-		newState = newState
-		throw new Error('This class method must be replaced by the Device class!')
-	}
-	clearFuture (clearAfterTime: number) {
-		// Clear any scheduled commands after this time
-		clearAfterTime = clearAfterTime
-		throw new Error('This class method must be replaced by the Device class!')
-	}
-	get canConnect (): boolean {
-		throw new Error('This class method must be replaced by the Device class!')
-	}
-	get connected (): boolean {
-		// Returns connection status
-		throw new Error('This class method must be replaced by the Device class!')
-	}
+	abstract handleState (newState: TimelineState)
+	abstract clearFuture (clearAfterTime: number)
+	abstract get canConnect (): boolean
+	abstract get connected (): boolean
 
 	/**
 	 * The makeReady method could be triggered at a time before broadcast
@@ -120,17 +101,11 @@ export abstract class Device extends EventEmitter implements IDevice {
 	get deviceId () {
 		return this._deviceId
 	}
-	get deviceName (): string {
-		// Return a human-readable name for this device
-		throw new Error('This class method must be replaced by the Device class!')
-	}
 	set deviceId (deviceId) {
 		this._deviceId = deviceId
 	}
-	get deviceType (): DeviceType {
-		// return DeviceType.ABSTRACT
-		throw new Error('This class method must be replaced by the Device class!')
-	}
+	abstract get deviceName (): string
+	abstract get deviceType (): DeviceType
 	get deviceOptions (): DeviceOptions {
 		return this._deviceOptions
 	}
