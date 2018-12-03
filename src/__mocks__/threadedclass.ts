a7a004380788bbc970cc8c27e3136dbb7911d887
Unknown <balte.dewit@gmail.com>
2018-12-03 06:36:52 +0100

    wip(multithreaded): mock device

--- src/__mocks__/threadedclass.ts
+++ src/__mocks__/threadedclass.ts
@@ -27,25 +27,47 @@ export type ThreadedClass<T> = IProxy & Promisify<T>
  * @param orgClass The class to be threaded
  * @param constructorArgs An array of arguments to be fed into the class constructor
  */
-export function threadedClass<T> (orgModule: string, orgClass: Function, constructorArgs: any[] ): Promise<ThreadedClass<T>> {
+export function threadedClass<T> (orgModule: string, orgClass: any, constructorArgs: any[]): Promise<ThreadedClass<T>> {
 
 	return new Promise((resolve, reject) => {
-		let c = Object.create(orgClass.prototype)
-		c.constructor.call(this, ...constructorArgs)
+		let c = new orgClass(...constructorArgs)
 		let proxy = new ProxyClass() as ThreadedClass<T>
-		for (const prop of Object.getOwnPropertyNames(orgClass)) {
-			if (!new Set(['constructor', 'prototype']).has(prop)) {
-				proxy[prop] = (...args: any) => new Promise(resolve => {
-					resolve(c[prop](...args))
-				})
+
+		let allProps = getAllProperties(c)
+		allProps.forEach((prop: string) => {
+			if ([
+				'constructor',
+				'windows',
+				'constructor',
+				'__defineGetter__',
+				'__defineSetter__',
+				'hasOwnProperty',
+				'__lookupGetter__',
+				'__lookupSetter__',
+				'isPrototypeOf',
+				'propertyIsEnumerable',
+				'toString',
+				'valueOf',
+				'__proto__',
+				'toLocaleString'
+			].indexOf(prop) !== -1) return
+			if (typeof c[prop] === 'function') {
+				proxy[prop] = (...args) => new Promise(resolve => resolve(c[prop](...args)))
+			} else {
+				proxy[prop] = () => new Promise(resolve => resolve(c[prop]))
 			}
-		}
-		for (const prop of Object.getOwnPropertyNames(orgClass.prototype)) {
-			proxy[prop] = (...args: any) => new Promise(resolve => {
-				resolve(orgClass.prototype[prop](...args))
-			})
-		}
-		proxy.on = (...args) => c.on(...args)
+		})
+
 		resolve(proxy)
 	})
+}
+
+function getAllProperties (obj: Object) {
+	let props: Array<string> = []
+
+	do {
+		props = props.concat(Object.getOwnPropertyNames(obj))
+		obj = Object.getPrototypeOf(obj)
+	} while (obj)
+	return props
 }
