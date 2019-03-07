5820aa183546517753f79f9b7a8bf74e8cbaf594
Johan Nyman <johan@nytamin.se>
2019-03-07 09:38:51 +0100

    feat: DoOnTime: add burst-mode (equaling old functionality) and in-order-mode (run commands in sequence)

--- src/doOnTime.ts
+++ src/doOnTime.ts
@@ -8,17 +8,27 @@ interface DoOrder {
 	args: any[]
 }
 
+export enum SendMode {
+	/** Send messages as quick as possible */
+	BURST = 1,
+	/** Send messages in order, wait for the previous message to be acknowledged before sending the next */
+	IN_ORDER = 1
+}
 export class DoOnTime extends EventEmitter {
-	getCurrentTime: () => number
+	getCurrentTime: () => Promise<number>
 	private _i: number = 0
 	private _queue: {[id: string]: DoOrder} = {}
 	private _checkQueueTimeout: any = 0
+	private _sendMode: SendMode
+	private _commandsToSendNow: (() => Promise<any>)[] = []
+	private _sendingCommands: boolean = false
 
-	constructor (getCurrentTime: () => number) {
+	constructor (getCurrentTime: () => Promise<number>, sendMode: SendMode = SendMode.BURST) {
 		super()
 		this.getCurrentTime = getCurrentTime
+		this._sendMode = sendMode
 	}
-	public queue (time, fcn: DoOrderFunction, ...args: any[]): string {
+	public queue (time: number, fcn: DoOrderFunction, ...args: any[]): string {
 		if (!(time > 0)) throw Error('time argument must be > 0')
 		if (!_.isFunction(fcn)) throw Error('fcn argument must be a function!')
 		let id = '_' + (this._i++)
@@ -28,7 +38,7 @@ export class DoOnTime extends EventEmitter {
 			args: args
 		}
 		this._checkQueueTimeout = setTimeout(() => {
-			this._checkQueue()
+			this._checkQueue().catch(e => this.emit('error', e))
 		},0)
 		return id
 	}
@@ -43,14 +53,14 @@ export class DoOnTime extends EventEmitter {
 			}
 		})
 	}
-	public clearQueueAfter (time) {
+	public clearQueueAfter (time: number) {
 		_.each(this._queue, (q: DoOrder, id: string) => {
 			if (q.time > time) {
 				this.remove(id)
 			}
 		})
 	}
-	public clearQueueNowAndAfter (time) {
+	public clearQueueNowAndAfter (time: number) {
 		_.each(this._queue, (q: DoOrder, id: string) => {
 			if (q.time >= time) {
 				this.remove(id)
@@ -61,34 +71,81 @@ export class DoOnTime extends EventEmitter {
 		this.clearQueueAfter(0) // clear all
 		clearTimeout(this._checkQueueTimeout)
 	}
-	private _checkQueue () {
+	private async _checkQueue () {
 		clearTimeout(this._checkQueueTimeout)
 
-		let now = this.getCurrentTime()
+		let now = await this.getCurrentTime()
 
 		let nextTime = now + 99999
 
 		_.each(this._queue, (o: DoOrder, id: string) => {
 			if (o.time <= now) {
-				try {
-					Promise.resolve(o.fcn(...o.args))
-					.catch((e) => {
+				this._commandsToSendNow.push(() => {
+					try {
+						return Promise.resolve(o.fcn(...o.args))
+					} catch (e) {
 						this.emit('error', e)
-					})
-				} catch (e) {
-					this.emit('error', e)
-				}
+						return Promise.reject(e)
+					}
+				})
 				this.remove(id)
 			} else {
 				if (o.time < nextTime) nextTime = o.time
 			}
 		})
-		// next check
+		// Go through the commands to be sent:
+		this._sendNextCommand()
+
+		// schedule next check:
 		let timeToNext = Math.min(1000,
 			nextTime - now
 		)
 		this._checkQueueTimeout = setTimeout(() => {
-			this._checkQueue()
+			this._checkQueue().catch(e => this.emit('error', e))
 		}, timeToNext)
+	}
+	private _sendNextCommand () {
+		if (this._sendingCommands) {
+			return
+		}
+		this._sendingCommands = true
+
+		try {
+			const commandToSend = this._commandsToSendNow.shift()
+			if (commandToSend) {
+				if (this._sendMode === SendMode.BURST) {
+					// send all at once:
+					commandToSend()
+					.catch((e) => {
+						this.emit('error', e)
+					})
+					this._sendingCommands = false
+					// send next message:
+					setTimeout(() => {
+						this._sendNextCommand()
+					}, 0)
+				} else { // SendMode.IN_ORDER
+					// send one, wait for it to finish, then send next:
+					commandToSend()
+					.catch((e) => {
+						this.emit('error', e)
+					})
+					.then(() => {
+						this._sendingCommands = false
+						// send next message:
+						this._sendNextCommand()
+					})
+					.catch((e) => {
+						this._sendingCommands = false
+						this.emit('error', e)
+					})
+				}
+			} else {
+				this._sendingCommands = false
+			}
+		} catch (e) {
+			this._sendingCommands = false
+			throw e
+		}
 	}
 }
