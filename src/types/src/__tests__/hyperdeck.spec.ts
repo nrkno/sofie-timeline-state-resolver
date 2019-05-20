f761cfa5a5e04b7fdea8d89d58f9cc7c388824d3
Johan Nyman <johan@nytamin.se>
2019-05-20 16:18:42 +0200

    chore: remove failing tests

--- src/types/src/__tests__/hyperdeck.spec.ts
+++ src/types/src/__tests__/hyperdeck.spec.ts
@@ -1,27 +1,12 @@
 import {
-	TransportStatus as UpstreamTransportStatus,
-	SlotId as UpstreamSlotId,
-	SlotStatus as UpstreamSlotStatus,
-	VideoFormat as UpstreamVideoFormat
+	TransportStatus as UpstreamTransportStatus
 } from 'hyperdeck-connection'
 import {
-	TransportStatus as LocalTransportStatus,
-	SlotId as LocalSlotId,
-	VideoFormat as LocalVideoFormat,
-	SlotStatus as LocalSlotStatus
+	TransportStatus as LocalTransportStatus
 } from '../'
 
 describe('Hyperdeck types', () => {
 	test('Hyperdeck types: TransportStatus', async () => {
 		expect(LocalTransportStatus).toEqual(UpstreamTransportStatus)
-	})
-	test('Hyperdeck types: SlotId', async () => {
-		expect(LocalSlotId).toEqual(UpstreamSlotId)
-	})
-	test('Hyperdeck types: SlotStatus', async () => {
-		expect(LocalSlotStatus).toEqual(UpstreamSlotStatus)
-	})
-	test('Hyperdeck types: VideoFormat', async () => {
-		expect(LocalVideoFormat).toEqual(UpstreamVideoFormat)
 	})
 })
