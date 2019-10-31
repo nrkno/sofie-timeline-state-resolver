43813b9266d2d83037d97ebadd24d16598954935
Johan Nyman <johan@nytamin.se>
2019-10-31 08:52:21 +0100

    fix: expose vizMSEDevice

--- src/index.ts
+++ src/index.ts
@@ -4,5 +4,6 @@ export * from './doOnTime'
 export { CasparCGDevice } from './devices/casparCG'
 export { HyperdeckDevice } from './devices/hyperdeck'
 export { QuantelDevice } from './devices/quantel'
+export { VizMSEDevice } from './devices/vizMSE'
 
 export * from './types/src'
