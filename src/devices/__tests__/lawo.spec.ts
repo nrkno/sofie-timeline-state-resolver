d20751b011cd87e2d6ec4084a6074ce787f5db4b
Balte de Wit <balte.dewit@gmail.com>
2020-06-24 10:09:09 +0200

    feat(lawo): new mapping and objects to do composites of fader values

--- src/devices/__tests__/lawo.spec.ts
+++ src/devices/__tests__/lawo.spec.ts
@@ -181,6 +181,401 @@ describe('Lawo', () => {
 		expect(commandReceiver0).toHaveBeenCalledTimes(3)
 		// no new commands should be sent, nothing is sent on object end
 	})
+	test('Lawo: Change volume on multiple faders', async () => {
+
+		const commandReceiver0: any = jest.fn(() => {
+			return Promise.resolve()
+		})
+		let myChannelMapping0: MappingLawo = {
+			device: DeviceType.LAWO,
+			deviceId: 'myLawo',
+			mappingType: MappingLawoType.SOURCE,
+			identifier: 'BASE'
+		}
+		let myChannelMapping1: MappingLawo = {
+			device: DeviceType.LAWO,
+			deviceId: 'myLawo',
+			mappingType: MappingLawoType.SOURCE,
+			identifier: 'BASE2'
+		}
+		let myChannelsMapping: MappingLawo = {
+			device: DeviceType.LAWO,
+			deviceId: 'myLawo',
+			mappingType: MappingLawoType.SOURCES
+		}
+		let myRetriggerMapping: MappingLawo = {
+			device: DeviceType.LAWO,
+			deviceId: 'myLawo',
+			mappingType: MappingLawoType.TRIGGER_VALUE
+		}
+		let myChannelMapping: Mappings = {
+			'lawo_c1_fader': myChannelMapping0,
+			'lawo_c2_fader': myChannelMapping1,
+			'lawo_base': myChannelsMapping,
+			'lawo_trigger': myRetriggerMapping
+		}
+
+		let myConductor = new Conductor({
+			initializeAsClear: true,
+			getCurrentTime: mockTime.getCurrentTime
+		})
+		await myConductor.setMapping(myChannelMapping)
+		await myConductor.init() // we cannot do an await, because setTimeout will never call without jest moving on.
+		await myConductor.addDevice('myLawo', {
+			type: DeviceType.LAWO,
+			options: {
+				host: '160.67.96.51',
+				port: 9000,
+				commandReceiver: commandReceiver0,
+
+				deviceMode: LawoDeviceMode.Ruby
+			}
+		})
+		await mockTime.advanceTimeToTicks(10100)
+
+		let deviceContainer = myConductor.getDevice('myLawo')
+		let device = deviceContainer.device as ThreadedClass<LawoDevice>
+
+		// Check that no commands has been scheduled:
+		expect(await device.queue).toHaveLength(0)
+		myConductor.timeline = [
+			{
+				id: 'obj0',
+				enable: {
+					start: mockTime.now - 1000, // 1 seconds in the past
+					duration: 2000
+				},
+				layer: 'lawo_base',
+				content: {
+					deviceType: DeviceType.LAWO,
+					type: TimelineContentTypeLawo.SOURCES,
+
+					sources: [{
+						mappingName: 'lawo_c1_fader',
+						value: -6
+					},{
+						mappingName: 'lawo_c2_fader',
+						value: -6
+					}]
+				}
+			},
+			{
+				id: 'obj1',
+				enable: {
+					start: mockTime.now + 500, // 0.5 seconds in the future
+					duration: 2000
+				},
+				layer: 'lawo_base',
+				content: {
+					deviceType: DeviceType.LAWO,
+					type: TimelineContentTypeLawo.SOURCES,
+
+					sources: [{
+						mappingName: 'lawo_c1_fader',
+						value: -4,
+						transitionDuration: 400
+					},{
+						mappingName: 'lawo_c2_fader',
+						value: -4,
+						transitionDuration: 400
+					}]
+				}
+			},
+			{
+				id: 'obj2',
+				enable: {
+					start: mockTime.now + 1000, // 1 seconds in the future
+					duration: 2000
+				},
+				layer: 'lawo_base',
+				content: {
+					deviceType: DeviceType.LAWO,
+					type: TimelineContentTypeLawo.SOURCES,
+
+					sources: [{
+						mappingName: 'lawo_c1_fader',
+						value: -4,
+						transitionDuration: 400
+					},{
+						mappingName: 'lawo_c2_fader',
+						value: -4,
+						transitionDuration: 400
+					}]
+				}
+			},
+			{
+				id: 'obj3',
+				enable: {
+					start: mockTime.now + 2000, // 2 seconds in the future
+					duration: 2000
+				},
+				layer: 'lawo_trigger',
+				content: {
+					deviceType: DeviceType.LAWO,
+					type: TimelineContentTypeLawo.TRIGGER_VALUE,
+
+					triggerValue: 'asdf' // only used for trigging new command sent
+				}
+			}
+		]
+
+		await mockTime.advanceTimeToTicks(10200)
+
+		expect(commandReceiver0).toHaveBeenCalledTimes(2)
+		expect(getMockCall(commandReceiver0, 0, 1)).toMatchObject(
+			{
+				// attribute: 'Motor dB Value',
+				type: TimelineContentTypeLawo.SOURCE,
+				value: -6,
+				path: 'Ruby.Sources.BASE.Fader.Motor dB Value'
+			}
+		)
+		expect(getMockCall(commandReceiver0, 0, 2)).toBeTruthy()
+		expect(getMockCall(commandReceiver0, 1, 1)).toMatchObject(
+			{
+				// attribute: 'Motor dB Value',
+				type: TimelineContentTypeLawo.SOURCE,
+				value: -6,
+				path: 'Ruby.Sources.BASE2.Fader.Motor dB Value'
+			}
+		)
+		expect(getMockCall(commandReceiver0, 1, 2)).toBeTruthy()
+
+		await mockTime.advanceTimeToTicks(11000)
+
+		expect(commandReceiver0).toHaveBeenCalledTimes(4)
+		expect(getMockCall(commandReceiver0, 2, 1)).toMatchObject(
+			{
+				// attribute: 'Motor dB Value',
+				type: TimelineContentTypeLawo.SOURCE,
+				value: -4,
+				transitionDuration: 400,
+				path: 'Ruby.Sources.BASE.Fader.Motor dB Value'
+			}
+		)
+		expect(getMockCall(commandReceiver0, 2, 2)).toBeTruthy() // context
+		expect(getMockCall(commandReceiver0, 3, 1)).toMatchObject(
+			{
+				// attribute: 'Motor dB Value',
+				type: TimelineContentTypeLawo.SOURCE,
+				value: -4,
+				transitionDuration: 400,
+				path: 'Ruby.Sources.BASE2.Fader.Motor dB Value'
+			}
+		)
+		expect(getMockCall(commandReceiver0, 3, 2)).toBeTruthy() // context
+
+		await mockTime.advanceTimeToTicks(11500)
+		expect(commandReceiver0).toHaveBeenCalledTimes(4)
+		// no new commands should have been sent, becuse obj2 is the same as obj1
+
+		await mockTime.advanceTimeToTicks(12500)
+		expect(commandReceiver0).toHaveBeenCalledTimes(6)
+		expect(getMockCall(commandReceiver0, 4, 1)).toMatchObject(
+			{
+				// attribute: 'Motor dB Value',
+				type: TimelineContentTypeLawo.SOURCE,
+				value: -4,
+				path: 'Ruby.Sources.BASE.Fader.Motor dB Value'
+			}
+		)
+		expect(getMockCall(commandReceiver0, 4, 2)).toMatch(/triggerValue/i) // context
+		expect(getMockCall(commandReceiver0, 5, 1)).toMatchObject(
+			{
+				// attribute: 'Motor dB Value',
+				type: TimelineContentTypeLawo.SOURCE,
+				value: -4,
+				path: 'Ruby.Sources.BASE2.Fader.Motor dB Value'
+			}
+		)
+		expect(getMockCall(commandReceiver0, 5, 2)).toMatch(/triggerValue/i) // context
+		await mockTime.advanceTimeToTicks(14500)
+		expect(commandReceiver0).toHaveBeenCalledTimes(6)
+		// no new commands should be sent, nothing is sent on object end
+	})
+	test('Lawo: Change volume on multiple faders with priorities', async () => {
+
+		const commandReceiver0: any = jest.fn(() => {
+			return Promise.resolve()
+		})
+		let myChannelMapping0: MappingLawo = {
+			device: DeviceType.LAWO,
+			deviceId: 'myLawo',
+			mappingType: MappingLawoType.SOURCE,
+			identifier: 'SOURCE1'
+		}
+		let myChannelMapping1: MappingLawo = {
+			device: DeviceType.LAWO,
+			deviceId: 'myLawo',
+			mappingType: MappingLawoType.SOURCE,
+			identifier: 'SOURCE2'
+		}
+		let myChannelsMapping: MappingLawo = {
+			device: DeviceType.LAWO,
+			deviceId: 'myLawo',
+			mappingType: MappingLawoType.SOURCES
+		}
+		let myRetriggerMapping: MappingLawo = {
+			device: DeviceType.LAWO,
+			deviceId: 'myLawo',
+			mappingType: MappingLawoType.TRIGGER_VALUE
+		}
+		let myChannelMapping: Mappings = {
+			'lawo_c1_fader': myChannelMapping0,
+			'lawo_c2_fader': myChannelMapping1,
+			'lawo_base': myChannelsMapping,
+			'lawo_trigger': myRetriggerMapping
+		}
+
+		let myConductor = new Conductor({
+			initializeAsClear: true,
+			getCurrentTime: mockTime.getCurrentTime
+		})
+		await myConductor.setMapping(myChannelMapping)
+		await myConductor.init() // we cannot do an await, because setTimeout will never call without jest moving on.
+		await myConductor.addDevice('myLawo', {
+			type: DeviceType.LAWO,
+			options: {
+				host: '160.67.96.51',
+				port: 9000,
+				commandReceiver: commandReceiver0,
+
+				deviceMode: LawoDeviceMode.Ruby
+			}
+		})
+		await mockTime.advanceTimeToTicks(10100)
+
+		let deviceContainer = myConductor.getDevice('myLawo')
+		let device = deviceContainer.device as ThreadedClass<LawoDevice>
+
+		// Check that no commands has been scheduled:
+		expect(await device.queue).toHaveLength(0)
+		myConductor.timeline = [
+			{
+				id: 'obj0',
+				enable: {
+					start: mockTime.now - 1000, // 1 seconds in the past
+					duration: 2000
+				},
+				layer: 'lawo_base',
+				content: {
+					deviceType: DeviceType.LAWO,
+					type: TimelineContentTypeLawo.SOURCES,
+
+					sources: [{
+						mappingName: 'lawo_c1_fader',
+						value: -6
+					},{
+						mappingName: 'lawo_c2_fader',
+						value: -6
+					}]
+				}
+			},
+			{
+				id: 'obj1',
+				enable: {
+					start: mockTime.now - 1000, // 1 seconds in the past
+					duration: 2000
+				},
+				layer: 'lawo_c1_fader',
+				content: {
+					deviceType: DeviceType.LAWO,
+					type: TimelineContentTypeLawo.SOURCE,
+
+					value: 0,
+					overridePriority: 1
+				}
+			},
+			{
+				id: 'obj2',
+				enable: {
+					start: mockTime.now + 500, // 0.5 seconds in the future
+					duration: 2000
+				},
+				layer: 'lawo_base',
+				content: {
+					deviceType: DeviceType.LAWO,
+					type: TimelineContentTypeLawo.SOURCES,
+
+					sources: [{
+						mappingName: 'lawo_c1_fader',
+						value: -4,
+						transitionDuration: 400
+					},{
+						mappingName: 'lawo_c2_fader',
+						value: -4,
+						transitionDuration: 400
+					}],
+					overridePriority: 1
+				}
+			},
+			{
+				id: 'obj3',
+				enable: {
+					start: mockTime.now + 500, // 0.5 seconds in the future
+					duration: 2000
+				},
+				layer: 'lawo_c1_fader',
+				content: {
+					deviceType: DeviceType.LAWO,
+					type: TimelineContentTypeLawo.SOURCE,
+
+					value: -12,
+					transitionDuration: 400
+				}
+			},
+		]
+
+		await mockTime.advanceTimeToTicks(10200)
+
+		expect(commandReceiver0).toHaveBeenCalledTimes(2)
+		expect(getMockCall(commandReceiver0, 0, 1)).toMatchObject(
+			{
+				// attribute: 'Motor dB Value',
+				type: TimelineContentTypeLawo.SOURCE,
+				value: 0,
+				path: 'Ruby.Sources.SOURCE1.Fader.Motor dB Value'
+			}
+		)
+		expect(getMockCall(commandReceiver0, 0, 2)).toBeTruthy()
+		expect(getMockCall(commandReceiver0, 1, 1)).toMatchObject(
+			{
+				// attribute: 'Motor dB Value',
+				type: TimelineContentTypeLawo.SOURCE,
+				value: -6,
+				path: 'Ruby.Sources.SOURCE2.Fader.Motor dB Value'
+			}
+		)
+		expect(getMockCall(commandReceiver0, 1, 2)).toBeTruthy()
+
+		await mockTime.advanceTimeToTicks(11000)
+
+		expect(commandReceiver0).toHaveBeenCalledTimes(4)
+		expect(getMockCall(commandReceiver0, 2, 1)).toMatchObject(
+			{
+				// attribute: 'Motor dB Value',
+				type: TimelineContentTypeLawo.SOURCE,
+				value: -4,
+				transitionDuration: 400,
+				path: 'Ruby.Sources.SOURCE1.Fader.Motor dB Value'
+			}
+		)
+		expect(getMockCall(commandReceiver0, 2, 2)).toBeTruthy() // context
+		expect(getMockCall(commandReceiver0, 3, 1)).toMatchObject(
+			{
+				// attribute: 'Motor dB Value',
+				type: TimelineContentTypeLawo.SOURCE,
+				value: -4,
+				transitionDuration: 400,
+				path: 'Ruby.Sources.SOURCE2.Fader.Motor dB Value'
+			}
+		)
+		expect(getMockCall(commandReceiver0, 3, 2)).toBeTruthy() // context
+
+		await mockTime.advanceTimeToTicks(14500)
+		expect(commandReceiver0).toHaveBeenCalledTimes(4)
+		// no new commands should be sent, nothing is sent on object end
+	})
 	test('Lawo: Set delay om main/pgm', async () => {
 
 		const commandReceiver0: any = jest.fn(() => {
