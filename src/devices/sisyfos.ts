d17a0b1af4510d8e5e363e01a4de25bd12db3d3e
Balte de Wit <contact@balte.nl>
2021-02-15 11:46:28 +0100

    fix(sisyfos): retrigger only channels that are mapped in Sofie

--- src/devices/sisyfos.ts
+++ src/devices/sisyfos.ts
@@ -23,7 +23,8 @@ import {
 	MappingSisyfosType,
 	TimelineObjSisyfosAny,
 	TimelineContentTypeSisyfos,
-	SisyfosChannelOptions
+	SisyfosChannelOptions,
+	MappingSisyfosChannel
 } from '../types/src/sisyfos'
 import {
 	SisyfosApi,
@@ -32,6 +33,8 @@ import {
 	SisyfosChannel,
 	SisyfosCommandType
 } from './sisyfosAPI'
+import Debug from "debug"
+const debug = Debug('timeline-state-resolver:sisyfos')
 
 export interface DeviceOptionsSisyfosInternal extends DeviceOptionsSisyfos {
 	options: (
@@ -205,7 +208,7 @@ export class SisyfosMessageDevice extends DeviceWithState<SisyfosState> implemen
 	get connected (): boolean {
 		return this._sisyfos.connected
 	}
-	getDeviceState (isDefaultState = true): SisyfosState {
+	getDeviceState (isDefaultState = true, mappings?: Mappings): SisyfosState {
 		let deviceStateFromAPI = this._sisyfos.state
 		const deviceState: SisyfosState = {
 			channels: {},
@@ -214,7 +217,11 @@ export class SisyfosMessageDevice extends DeviceWithState<SisyfosState> implemen
 
 		if (!deviceStateFromAPI) deviceStateFromAPI = deviceState
 
-		for (const ch of Object.keys(deviceStateFromAPI.channels)) {
+		const channels = mappings ? Object.values(mappings || {})
+			.filter((m: MappingSisyfos) => m.mappingType === MappingSisyfosType.CHANNEL)
+			.map((m: MappingSisyfosChannel) => m.channel) : Object.keys(deviceStateFromAPI.channels)
+
+		for (const ch of channels) {
 
 			const channelFromAPI = deviceStateFromAPI.channels[ch]
 
@@ -250,7 +257,7 @@ export class SisyfosMessageDevice extends DeviceWithState<SisyfosState> implemen
 	 * @param state
 	 */
 	convertStateToSisyfosState (state: TimelineState, mappings: Mappings) {
-		const deviceState: SisyfosState = this.getDeviceState()
+		const deviceState: SisyfosState = this.getDeviceState(true, mappings)
 
 		_.each(state.layers, (tlObject, layerName) => {
 			const layer = tlObject as ResolvedTimelineObjectInstance & TimelineObjSisyfosAny
@@ -390,6 +397,7 @@ export class SisyfosMessageDevice extends DeviceWithState<SisyfosState> implemen
 
 			if ((newOscSendState.triggerValue && newOscSendState.triggerValue !== oldOscSendState.triggerValue)) { // || (!oldChannel && Number(index) >= 0)) {
 				// push commands for everything
+				debug('reset channel ' + index)
 				commands.push({
 					context: `Channel ${index} reset`,
 					content: {
@@ -403,6 +411,7 @@ export class SisyfosMessageDevice extends DeviceWithState<SisyfosState> implemen
 			}
 
 			if (oldChannel && oldChannel.pgmOn !== newChannel.pgmOn) {
+				debug(`Channel ${index} pgm goes from "${oldChannel.pgmOn}" to "${newChannel.pgmOn}"`)
 				commands.push({
 					context: `Channel ${index} pgm goes from "${oldChannel.pgmOn}" to "${newChannel.pgmOn}"`,
 					content: {
@@ -415,6 +424,7 @@ export class SisyfosMessageDevice extends DeviceWithState<SisyfosState> implemen
 			}
 
 			if (oldChannel && oldChannel.pstOn !== newChannel.pstOn) {
+				debug(`Channel ${index} pst goes from "${oldChannel.pstOn}" to "${newChannel.pstOn}"`)
 				commands.push({
 					context: `Channel ${index} pst goes from "${oldChannel.pstOn}" to "${newChannel.pstOn}"`,
 					content: {
@@ -427,6 +437,7 @@ export class SisyfosMessageDevice extends DeviceWithState<SisyfosState> implemen
 			}
 
 			if (oldChannel && oldChannel.faderLevel !== newChannel.faderLevel) {
+				debug(`change faderLevel ${index}: "${newChannel.faderLevel}"`)
 				commands.push({
 					context: 'faderLevel change',
 					content: {
@@ -440,6 +451,7 @@ export class SisyfosMessageDevice extends DeviceWithState<SisyfosState> implemen
 
 			newChannel.label = newChannel.label || (oldChannel ? oldChannel.label : '')
 			if (oldChannel && newChannel.label !== '' && oldChannel.label !== newChannel.label) {
+				debug(`set label on fader ${index}: "${newChannel.label}"`)
 				commands.push({
 					context: 'set label on fader',
 					content: {
@@ -452,6 +464,7 @@ export class SisyfosMessageDevice extends DeviceWithState<SisyfosState> implemen
 			}
 
 			if (oldChannel && oldChannel.visible !== newChannel.visible) {
+				debug(`Channel ${index} Visibility goes from "${oldChannel.visible}" to "${newChannel.visible}"`)
 				commands.push({
 					context: `Channel ${index} Visibility goes from "${oldChannel.visible}" to "${newChannel.visible}"`,
 					content: {
