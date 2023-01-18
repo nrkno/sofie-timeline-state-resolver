c67c36929150c42db09b928ae1918e2a45482df9
Julian Waller <git@julusian.co.uk>
2023-01-18 11:30:03 +0000

    wip: generate translations

--- packages/timeline-state-resolver/scripts/translation/jsonLexer.mjs
+++ packages/timeline-state-resolver/scripts/translation/jsonLexer.mjs
@@ -13,20 +13,41 @@ export class JsonLexer extends BaseLexer {
 		try {
 			const obj = JSON.parse(content)
 
+			// The root could be an object in places without a meta-schema (eg options)
+			processObject(keys, obj)
+
+			// actions are nested
 			if ('actions' in obj) {
 				obj.actions.forEach((action) => {
 					action.name && keys.push(action.name)
-					if (action.payload && action.payload.type === 'object') {
-						Object.values(action.payload.properties || {}).forEach((prop) => prop.title && keys.push(prop.title))
-					}
+					processObject(keys, action.payload)
+				})
+			}
+
+			// mappings are nested
+			if ('mappings' in obj) {
+				Object.values(obj.mappings).forEach((mapping) => {
+					processObject(keys, mapping)
 				})
 			}
 		} catch (e) {
 			console.error(`File "${filename}" contains invalid json`)
 		}
 
-		console.log(keys) // haha just figuring wtf is going on
+		// console.log(filename, keys) // haha just figuring wtf is going on
 
 		return keys.map((k) => ({ key: k }))
+	}
+}
+
+function processObject(keys, obj) {
+	if (obj && obj.type === 'object') {
+		for (const prop of Object.values(obj.properties || {})) {
+			if (prop['ui:title']) keys.push(prop['ui:title'])
+			if (prop['ui:summaryTitle']) keys.push(prop['ui:summaryTitle'])
+			if (prop['ui:description']) keys.push(prop['ui:description'])
+
+			processObject(prop)
+		}
 	}
 }
