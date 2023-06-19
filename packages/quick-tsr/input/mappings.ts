afade265766e07aed9e9dfa0c743d0e45b85e732
Julian Waller <git@julusian.co.uk>
2023-06-19 10:00:23 +0100

    chore: fix type error

--- packages/quick-tsr/input/mappings.ts
+++ packages/quick-tsr/input/mappings.ts
@@ -1,14 +1,17 @@
-import { DeviceType, MappingCasparCG } from 'timeline-state-resolver'
+import { DeviceType, Mapping, MappingCasparCGType, SomeMappingCasparCG } from 'timeline-state-resolver'
 import { literal } from 'timeline-state-resolver/dist/devices/device'
 import type { TSRInput } from '../src'
 
 export const input: TSRInput = {
 	mappings: {
-		casparLayer0: literal<MappingCasparCG>({
+		casparLayer0: literal<Mapping<SomeMappingCasparCG>>({
 			device: DeviceType.CASPARCG,
 			deviceId: 'caspar0',
-			channel: 1,
-			layer: 10,
+			options: {
+				mappingType: MappingCasparCGType.Layer,
+				channel: 1,
+				layer: 10,
+			},
 		}),
 	},
 }
