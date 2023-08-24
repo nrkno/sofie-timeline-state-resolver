acafdd463ec326875a81d0362a3b55bfd164f18d
Mint de Wit <mint@superfly.tv>
2023-08-24 16:03:28 +0200

    chore: add deviceinstance unit tests

---
+++ packages/timeline-state-resolver/src/service/__tests__/deviceInstance.spec.ts
@@ -0,0 +1,162 @@
+import { DeviceType, StatusCode } from 'timeline-state-resolver-types'
+import { DeviceInstanceWrapper } from '../DeviceInstance'
+import { ActionExecutionResultCode } from 'timeline-state-resolver-types'
+import { t } from '../../lib'
+import { DevicesDict } from '../devices'
+
+const StateHandler = {
+	terminate: jest.fn(),
+	clearFutureStates: jest.fn(),
+	handleState: jest.fn(),
+	setCurrentState: jest.fn(),
+	clearFutureAfterTimestamp: jest.fn(),
+}
+jest.mock('../stateHandler', () => ({
+	StateHandler: class Statehandler {
+		terminate = StateHandler.terminate
+		clearFutureStates = StateHandler.clearFutureStates
+		handleState = jest.fn(async () => {
+			// some shenanigans required to get the mock impl to work
+			StateHandler.handleState()
+			return Promise.resolve()
+		})
+		setCurrentState = StateHandler.setCurrentState
+		clearFutureAfterTimestamp = StateHandler.clearFutureAfterTimestamp
+	},
+}))
+
+const AbstractDeviceMock = {
+	init: jest.fn(),
+	terminate: jest.fn(),
+	action: jest.fn(),
+	convertTimelineStateToDeviceState: jest.fn(),
+	getStatus: jest.fn(),
+	diffStates: jest.fn(),
+	sendCommand: jest.fn(),
+	on: jest.fn(),
+}
+jest.mock('../../integrations/abstract/index', () => ({
+	AbstractDevice: class AbstractDevice {
+		actions = {
+			action: AbstractDeviceMock.action,
+		}
+		init = AbstractDeviceMock.init
+		terminate = AbstractDeviceMock.terminate
+		convertTimelineStateToDeviceState = AbstractDeviceMock.convertTimelineStateToDeviceState
+		getStatus = () => {
+			AbstractDeviceMock.getStatus()
+			return { statusCode: StatusCode.GOOD, messages: [] }
+		}
+		diffStates = AbstractDeviceMock.diffStates
+		sendCommand = AbstractDeviceMock.sendCommand
+		on = AbstractDeviceMock.on
+	},
+}))
+
+function getDeviceInstance(): DeviceInstanceWrapper {
+	return new DeviceInstanceWrapper('wrapper0', { type: DeviceType.ABSTRACT }, () => Date.now())
+}
+
+describe('DeviceInstance', () => {
+	afterEach(() => {
+		jest.resetAllMocks()
+	})
+
+	test('constructor', () => {
+		const dev = getDeviceInstance()
+		expect(dev).toBeTruthy()
+
+		// todo - check that the statehandler was created as expected
+	})
+
+	test('initDevice', async () => {
+		const dev = getDeviceInstance()
+		await dev.initDevice()
+		expect(AbstractDeviceMock.init).toHaveBeenCalled()
+	})
+
+	test('terminate', async () => {
+		const dev = getDeviceInstance()
+		await dev.terminate()
+
+		expect(StateHandler.terminate).toHaveBeenCalled()
+		expect(AbstractDeviceMock.terminate).toHaveBeenCalled()
+	})
+
+	describe('executeAction', () => {
+		test('execute action', async () => {
+			const dev = getDeviceInstance()
+			await dev.executeAction('action', { payload: 1 })
+
+			expect(AbstractDeviceMock.action).toHaveBeenCalledWith('action', { payload: 1 })
+		})
+
+		test('unknown id', async () => {
+			const dev = getDeviceInstance()
+			const result = await dev.executeAction('doesnt exist', { payload: 1 })
+
+			expect(AbstractDeviceMock.action).not.toHaveBeenCalled()
+			expect(result).toEqual({
+				result: ActionExecutionResultCode.Error,
+				response: t('Action "{{id}}" not found', { id: 'doesnt exist' }),
+			})
+		})
+	})
+
+	test('handleState', () => {
+		const dev = getDeviceInstance()
+		dev.handleState({ time: 100, layers: {}, nextEvents: [] }, {})
+
+		expect(StateHandler.handleState).toHaveBeenCalled()
+	})
+
+	test('clearFuture', () => {
+		const dev = getDeviceInstance()
+		dev.clearFuture(1)
+
+		expect(StateHandler.clearFutureAfterTimestamp).toHaveBeenCalledWith(1)
+	})
+
+	test('getDetails', () => {
+		const dev = getDeviceInstance()
+		const details = dev.getDetails()
+
+		expect(details).toEqual({
+			deviceId: 'wrapper0',
+			deviceType: DeviceType.ABSTRACT,
+			deviceName: DevicesDict[DeviceType.ABSTRACT].deviceName('wrapper0', {}),
+			instanceId: undefined,
+			startTime: expect.anything(),
+
+			supportsExpectedPlayoutItems: false,
+			canConnect: DevicesDict[DeviceType.ABSTRACT].canConnect,
+		})
+	})
+
+	test('getStatus', () => {
+		const dev = getDeviceInstance()
+		const status = dev.getStatus()
+
+		expect(AbstractDeviceMock.getStatus).toHaveBeenCalled()
+		expect(status).toEqual({
+			statusCode: StatusCode.GOOD,
+			messages: [],
+			active: false, // because it has no mappings
+		})
+
+		dev.handleState(
+			{ time: 1, layers: {}, nextEvents: [] },
+			{ test: { device: DeviceType.ABSTRACT, deviceId: 'wrapper0', options: {} } }
+		)
+		const status2 = dev.getStatus()
+
+		expect(AbstractDeviceMock.getStatus).toHaveBeenCalledTimes(2)
+		expect(status2).toEqual({
+			statusCode: StatusCode.GOOD,
+			messages: [],
+			active: true, // because it has mappings now
+		})
+	})
+
+	// todo - test event handlers
+})
