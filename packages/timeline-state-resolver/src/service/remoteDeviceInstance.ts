e7ee05d417e008b0d9d0783adf92806892974fae
Mint de Wit <mint@superfly.tv>
2023-08-25 08:12:31 +0200

    chore: type assertion for incompatible type

--- packages/timeline-state-resolver/src/service/remoteDeviceInstance.ts
+++ packages/timeline-state-resolver/src/service/remoteDeviceInstance.ts
@@ -125,7 +125,7 @@ export class RemoteDeviceInstance<
 		container._device = await threadedClass<DeviceInstanceWrapper, typeof DeviceInstanceWrapper>(
 			'../../dist/service/DeviceInstance.js',
 			'DeviceInstanceWrapper',
-			[deviceId, getCurrentTime(), deviceOptions as DeviceOptionsAnyInternal, getCurrentTime],
+			[deviceId, getCurrentTime(), deviceOptions as DeviceOptionsAnyInternal, getCurrentTime as any], // any because callbacks can't be casted
 			threadConfig
 		)
 
