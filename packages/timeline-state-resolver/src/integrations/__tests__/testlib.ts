771796eaa92cfc9f7133a3b8b99c528c4b0f0965
Mint de Wit <mint@superfly.tv>
2023-11-30 10:43:50 +0100

    chore: fix tests

--- packages/timeline-state-resolver/src/integrations/__tests__/testlib.ts
+++ packages/timeline-state-resolver/src/integrations/__tests__/testlib.ts
@@ -17,6 +17,7 @@ export function getDeviceContext(): DeviceContextAPI {
 		updateMediaObject: jest.fn(),
 		clearMediaObjects: jest.fn(),
 		timeTrace: jest.fn(),
-		resetState: jest.fn(),
+		resetState: jest.fn(async () => Promise.resolve()),
+		resetToState: jest.fn(async () => Promise.resolve()),
 	}
 }
