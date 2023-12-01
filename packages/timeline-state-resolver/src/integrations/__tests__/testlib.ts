83a8945e8724ed4abccf17cd6a79eddac1c83b36
Mint de Wit <mint@superfly.tv>
2023-12-01 14:33:26 +0100

    chore: update tests

--- packages/timeline-state-resolver/src/integrations/__tests__/testlib.ts
+++ packages/timeline-state-resolver/src/integrations/__tests__/testlib.ts
@@ -2,7 +2,7 @@ import { DeviceContextAPI } from '../../service/device'
 
 /** A default context for devices used in unit tests */
 
-export function getDeviceContext(): DeviceContextAPI {
+export function getDeviceContext(): DeviceContextAPI<any> {
 	return {
 		logger: {
 			error: jest.fn(),
