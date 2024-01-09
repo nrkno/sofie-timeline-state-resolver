1d8120dc8175a0002e8bea1df431cf1592e04035
Mint de Wit <mint@superfly.tv>
2024-01-09 08:54:46 +0100

    Revert "chore: quicktsr stuff"
    This reverts commit bcd87a6751c504ac0aa5770c565e8b765e13dbce.

--- packages/quick-tsr/input/devices.ts
+++ packages/quick-tsr/input/devices.ts
@@ -3,19 +3,11 @@ import { DeviceType } from 'timeline-state-resolver'
 
 export const input: TSRInput = {
 	devices: {
-		// caspar0: {
-		// 	type: DeviceType.CASPARCG,
-		// 	options: {
-		// 		host: '127.0.0.1',
-		// 		port: 5250,
-		// 	},
-		// },
-		obs0: {
-			type: DeviceType.OBS,
+		caspar0: {
+			type: DeviceType.CASPARCG,
 			options: {
 				host: '127.0.0.1',
-				port: 4455,
-				password: 'PT7WXMT3iRTUnche',
+				port: 5250,
 			},
 		},
 	},
